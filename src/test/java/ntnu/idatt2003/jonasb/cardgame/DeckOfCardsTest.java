4e51d5cbcf7d24b53fcb73bae65e21bbae32e3d0
Wizard764 <150451202+Wizard764@users.noreply.github.com>
2024-03-05 12:19:55 +0100

    Added Unit tests for DeckOfCards.

---
+++ src/test/java/ntnu/idatt2003/jonasb/cardgame/DeckOfCardsTest.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2003.jonasb.cardgame;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for DeckOfCards.
+ */
+public class DeckOfCardsTest {
+  @Test
+  public void testCreateDeck() {
+    DeckOfCards deck = new DeckOfCards();
+  }
+}
